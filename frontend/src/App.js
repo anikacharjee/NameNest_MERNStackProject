77478068557dd8284fa9d25cb3f06d6d0e0e10bc
Anik Acharjee <anikacharjee@users.noreply.github.com>
2025-11-13 05:30:46 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NameForm from './components/NameForm';
+
+function App() {
+    return (
+        <div className="App">
+            <NameForm />
+        </div>
+    );
+}
+
+export default App;
